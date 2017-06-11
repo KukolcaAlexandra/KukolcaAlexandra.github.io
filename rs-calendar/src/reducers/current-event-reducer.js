ae366114402084c0d90111fa8b3bc1e40a6d1a22
KukolcaAlexandra <Kukolca_shura@mail.ru>
2017-06-12 00:12:41 +0300

    rs-calendar/feature: add new version of calendar

--- rs-calendar/src/reducers/current-event-reducer.js
+++
@@ -1,9 +0,0 @@
-function currentEventReducer(state = -1, action){
-	switch (action.type) {
-		case 'SET_CURRENT_EVENT_ID':
-			return action.payload;
-		default: return state;
-	}
-}
-
-export default currentEventReducer;
